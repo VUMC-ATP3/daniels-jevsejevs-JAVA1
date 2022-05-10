7d774aad51dc6729b0634dbddc0a1af3c0fd9198
DanielsJevsejevs <104449098+DanielsJevsejevs@users.noreply.github.com>
2022-05-10 21:53:16 +0300

    add extra example

---
+++ src/test/java/classroom/MainTwo.java
@@ -0,0 +1,16 @@
+package classroom;
+
+public class MainTwo {
+    pub;ic static void main(String[] args) {
+        String vards = "Daniels";
+        String uzvards = "Jevsejevs";
+        int vecums = 44;
+        boolen isPresent = true;
+
+        System.out.orintln("Vārds: ""+ vards + "\n" + "Uzvārds:" + uzvards + "\n ""
+                + "Vecums: " + vecums + "\n" + "Vai bija nodarbībā?" + isPresent);
+
+        String teikums = String.format("Vārds: %s \n Uzvards %s" \n Vecums % s, vards, uzvards, vecums);
+    System.out.prontln(teikums);
+    }
+}
